d7d4d3642aa0e5702354185896100073d09aa218
SO <swoconnor2@gmail.com>
2020-09-28 00:31:49 -0700

    Added icon reset if opt-out not found

--- background.js
+++ background.js
@@ -1,5 +1,13 @@
+// Listen for search results
 browser.runtime.onMessage.addListener(updateIcon);
 
-function updateIcon(){
-    browser.browserAction.setIcon({path: "icons/test1.png"});
+// Update icon to show search results to user
+function updateIcon(response){
+    if(response.linkDetected == "yes"){
+        browser.browserAction.setIcon({path: "icons/test1.png"});
+    }
+    else{
+        browser.browserAction.setIcon({path: "icons/test.png"});
+    }
+    
 }
--- check_for_link.js
+++ check_for_link.js
@@ -1,6 +1,17 @@
+
+// Search page for CCPA opt-out link
 var pageElements = document.getElementsByTagName('*');
+var linkDetected = false;
 for(var i = 0; i < pageElements.length; i++) {
     if(pageElements[i].innerHTML.toLowerCase().search('do not sell') != -1) {
-        browser.runtime.sendMessage({});
+        linkDetected = true;
     }
+}
+
+// Send result to background script
+if(linkDetected){
+    browser.runtime.sendMessage({linkDetected: "yes"});
+}
+else{
+    browser.runtime.sendMessage({linkDetected: "no"});
 }
