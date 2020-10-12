6322b26f7dd94436cb583d051003203bf9e22942
SO <swoconnor2@gmail.com>
2020-10-12 00:33:27 -0700

    Changed popup display to be triggered by link, added functions for buttons.

--- check_for_link.js
+++ check_for_link.js
@@ -1,56 +1,91 @@
-// Try all: unset, or all : initial for stylesheet
+/**
+ * Content script to detect CCPA opt-out links.  Searches the page for
+ * such a link, then displays a notification if found.
+ */
 
-
-    // Add extension stylesheet
-    var link = document.createElement('link');
-    link.setAttribute('rel', 'stylesheet');
-    link.setAttribute('type', 'text/css');
-    link.setAttribute('href', 'https://www.gitcdn.xyz/repo/swow2015/Who-sSellingMyInfo-/master/popup.css');
-    link.setAttribute('href', 'popup.css');
-    document.getElementsByTagName('HEAD')[0].appendChild(link);
-
+/**
+ * Function to display notification
+ */
+function displayPopup(){
+    console.log('displaying popup');
     // Create popup
     var popup = document.createElement('div');
     popup.innerHTML = '<b>This website sells your personal information.  Opt out below. </b>';
-    popup.setAttribute('class', 'CCPAPopup');
-    popup.setAttribute('id', 'CCPA');
+    popup.setAttribute('id', 'CCPAPopup');
 
-    // Create x
+    // Create 'X' button
     var close = document.createElement('span');
     close.innerHTML = 'x';
-    close.setAttribute('class', 'CCPAClose');
+    close.setAttribute('id', 'CCPAClose');
+    close.setAttribute('onclick', 'close();');
     popup.appendChild(close);
 
     // Create more info link
-    var moreinfo = document.createElement('a');
-    moreinfo.setAttribute('class', 'CCPAMoreInfo');
-    moreinfo.setAttribute('href', 'http://www.google.com/');
-    moreinfo.innerHTML = 'More Info';
-    popup.appendChild(moreinfo);
+    var moreInfo = document.createElement('a');
+    moreInfo.setAttribute('id', 'CCPAMoreInfo');
+    moreInfo.setAttribute('href', 'http://www.google.com/');
+    moreInfo.innerHTML = 'More Info';
+    popup.appendChild(moreInfo);
 
     // Create button
     var button = document.createElement('button');
-    button.setAttribute('class', 'CCPAButton');
+    button.setAttribute('id', 'CCPAButton');
+    button.setAttribute('onclick', 'linkReference.click();');
     button.innerHTML = "Don't Sell My Personal Information";
     popup.appendChild(button);
 
     // Add popup to document
     document.getElementsByTagName('BODY')[0].appendChild(popup);
+    console.log('popup displayed');
+}
 
-// Search page for CCPA opt-out link
+/**
+ * Function to close notification when 'x' button is clicked
+ */
+function close(){
+    var popup = document.getElementById('CCPAPopup');
+    var close = document.getElementById('CCPAClose');
+    var moreInfo = document.getElementById('CCPAMoreInfo');
+    var button = document.getElementById('CCPAButton');
+    popup.style.display = 'none';
+    close.style.display = 'none';
+    moreInfo.style.display = 'none';
+    button.style.display = 'none';
+}
+
+/**
+ * Main script code: searches page for link, displays notification, sends
+ * results back to background script.
+ */
+
+// Add notification stylesheet
+var link = document.createElement('link');
+link.setAttribute('rel', 'stylesheet');
+link.setAttribute('type', 'text/css');
+link.setAttribute('href', 'https://www.gitcdn.xyz/repo/swow2015/Who-sSellingMyInfo-/master/popup.css');
+document.getElementsByTagName('head')[0].appendChild(link);
+
+// Search page elements for CCPA opt-out link
 var pageElements = document.getElementsByTagName('*');
 var linkDetected = false;
 for(var i = 0; i < pageElements.length; i++) {
+
+    // If opt-out link is found, display notification
     if(pageElements[i].innerHTML.toLowerCase().search('do not sell') != -1
         || pageElements[i].innerHTML.toLowerCase().search('don\'t sell') != -1) {
+        console.log('link found');
+        var linkReference = pageElements[i];
         linkDetected = true;
+        displayPopup();
     }
 }
 
-// Send result to background script
+// Send search result to background script
 if(linkDetected){
     browser.runtime.sendMessage({linkDetected: "yes"});
 }
 else{
     browser.runtime.sendMessage({linkDetected: "no"});
 }
+
+console.log('finished');
