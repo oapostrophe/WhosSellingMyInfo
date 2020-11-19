fcb12987b2e5899f08fa94c695d4e683cef07f3a
2br-2b <847785bd-d466-47cd-a536-eae4096d241d@anonaddy.me>
2020-11-19 08:37:11 -0500

    Added percentage calculator

--- popup/popup.js
+++ popup/popup.js
@@ -1,17 +1,25 @@
 var browser = browser || chrome;
 
+var num_detected = 0;
+var num_total = 0;
+
 browser.storage.local.get(["detected"], function (result) {
     if (result.detected != null) {
-        document.getElementById("detected").textContent = result.detected;
-    } else {
-        document.getElementById("detected").textContent = 0;
+        num_detected = result.detected;
     }
+    document.getElementById("detected").textContent = num_detected;
+
+    browser.storage.local.get(["total"], function (result) {
+        if (result.total != null) {
+            num_total = result.total;
+        }
+        document.getElementById("total").textContent = num_total;
+
+        var percentage = Math.round(num_detected / num_total * 100);
+        document.getElementById("percentage").textContent = percentage;
+
+    });
 });
 
-browser.storage.local.get(["total"], function (result) {
-    if (result.total != null) {
-        document.getElementById("total").textContent = result.total;
-    } else {
-        document.getElementById("total").textContent = 0;
-    }
-});
+
+
