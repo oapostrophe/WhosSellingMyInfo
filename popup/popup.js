d7c4f95e6d5021f5fc6fce4ddcf68e4552708996
2br-2b <847785bd-d466-47cd-a536-eae4096d241d@anonaddy.me>
2020-11-19 08:25:40 -0500

    Fixed some bugs

--- popup/popup.js
+++ popup/popup.js
@@ -1,15 +1,15 @@
 var browser = browser || chrome;
 
-chrome.storage.local.get(["detected"], function (result) {
-    if (!result.detected == null) {
+browser.storage.local.get(["detected"], function (result) {
+    if (result.detected != null) {
         document.getElementById("detected").textContent = result.detected;
     } else {
         document.getElementById("detected").textContent = 0;
     }
 });
 
-chrome.storage.local.get(["total"], function (result) {
-    if (!result.total == null) {
+browser.storage.local.get(["total"], function (result) {
+    if (result.total != null) {
         document.getElementById("total").textContent = result.total;
     } else {
         document.getElementById("total").textContent = 0;
