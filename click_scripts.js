cb12380d3445370921a82c647f089b1147ec0e08
SO <swoconnor2@gmail.com>
2021-07-16 16:41:38 -0700

    added license

--- click_scripts.js
+++ click_scripts.js
@@ -6,10 +6,7 @@
 var browser = browser || chrome;
 
 try {
-    /**
-     * Close notification when 'X' button is clicked.
-    */
-
+    // Close notification when 'X' button is clicked.
     document.getElementById('CCPAClose').onclick = function () {
         // console.log("closed")
         var popup = document.getElementById('CCPAPopup');
@@ -22,15 +19,10 @@ try {
         button.style.display = 'none';
     }
 
-    /**
-     * Find and click CCPA opt-out link
-     */
+    // Find and click CCPA opt-out link
     document.getElementById('CCPAButton').onclick = function () {
-        // console.log("stopped sale of personal info");
-
         var pageElements = document.getElementsByTagName('*');
         for (var i = 0; i < pageElements.length; i++) {
-
             // If opt-out link is found, click it.
             if (pageElements[i].innerHTML.toLowerCase().search('do not sell') != -1
                 || pageElements[i].innerHTML.toLowerCase().search('don\'t sell') != -1) {
@@ -39,9 +31,7 @@ try {
         }
     }
 } catch (err) {
-
     if (err.name != "TypeError") {
         console.error(err);
     }
-
 }
